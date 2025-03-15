667fb7eb3f8723bf4b007b4b544200f6f2ef0ee6
lizyyaaa <150126847+lizyyaaa@users.noreply.github.com>
2025-03-15 10:44:24 +0700

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -231,23 +231,23 @@ if nav == "🏠 Home":
             elif col in ["date_start", "tgl_kunjungan"]:
                 input_manual[col] = st.date_input(label, value=datetime.today())
             # Kolom yang memiliki opsi di option_dict
-           elif col in option_dict:
-                options = option_dict[col]
-                if options:
-                    # Khusus untuk type_tb, tambahkan opsi manual
-                    if col == "type_tb":
-                        pilihan_lainnya = st.selectbox(label, options + ["Lainnya (ketik sendiri)"])
-                        if pilihan_lainnya == "Lainnya (ketik sendiri)":
-                            input_manual[col] = st.text_input("Masukkan keterangan TB:", value="")
+            elif col in option_dict:
+                    options = option_dict[col]
+                    if options:
+                        # Khusus untuk type_tb, tambahkan opsi manual
+                        if col == "type_tb":
+                            pilihan_lainnya = st.selectbox(label, options + ["Lainnya (ketik sendiri)"])
+                            if pilihan_lainnya == "Lainnya (ketik sendiri)":
+                                input_manual[col] = st.text_input("Masukkan keterangan TB:", value="")
+                            else:
+                                input_manual[col] = pilihan_lainnya
                         else:
-                            input_manual[col] = pilihan_lainnya
+                            input_manual[col] = st.selectbox(label, options)
                     else:
-                        input_manual[col] = st.selectbox(label, options)
-                else:
+                        input_manual[col] = st.text_input(label, value="")
+             else:
+                    # Kolom lainnya default ke text_input
                     input_manual[col] = st.text_input(label, value="")
-            else:
-                # Kolom lainnya default ke text_input
-                input_manual[col] = st.text_input(label, value="")
         
         submitted_manual = st.form_submit_button("Submit Data Manual Tambahan")
     
